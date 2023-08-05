fe3f572a52bf91ffb6dcfbe0e4b725fa32e62d16
jiyun <sss9890@naver.com>
2023-08-05 14:24:39 +0900

    codeup

--- 코드업 100제/1019.cpp
+++ 코드업 100제/1019.cpp
@@ -1,42 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int x = 2;
-	int y = 2;
-	int a[11][11] ={};
-
-	for(int i=1; i<=10; i++)
-	{
-		for(int j=1; j<=10; j++)
-		{
-			scanf("%d ", &a[i][j]);
-		}
-	}	
-	
-	while(a[x][y] !=2)
-	{
-		a[x][y] = 9;
-
-		if(a[x][y+1] != 1)
-		{
-			y++;
-		}
-		else
-		{
-			x++;
-		}
-	}
-
-	a[x][y] = 9;
-
-	for(int i=1; i<=10; i++)
-	{
-		for(int j=1; j<=10; j++)
-		{
-			printf("%d ", a[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
+    int a,b,c;
+    scanf("%04d.%02d.%02d",&a,&b,&c);
+    printf("%04d.%02d.%02d",a,b,c);
+    return 0;
 }
---
+++ 코드업 100제/1053.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) 
+{
+    int a, b;
+
+    scanf("%d", &a);
+    printf("%d", !a);
+
+    return 0;
+}
